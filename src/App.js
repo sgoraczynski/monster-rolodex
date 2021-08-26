2cb9e79885cf913380794cf6a9c33fa5fddbc41e
sgoraczynski <s.goraczynski@interia.pl>
2021-08-26 14:49:06 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }));
+  } //we fetch users from web, we translate it via json so js can understand  it,
+    // and we take fetched users and setState of monsters array, to become
+    // a users array hahah lol xd, so easy...
+
+
+  render() {
+    return (
+      <div className='App'>
+        {this.state.monsters.map(monster => (
+            <h1 key={monster.id}> {monster.name} </h1>
+            )
+          )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
